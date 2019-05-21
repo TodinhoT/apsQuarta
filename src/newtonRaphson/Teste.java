f3d4bf5fe786f8f9bd52f3a613075fdf12968605
F.Massaharu <felipe-massaharu-oya@hotmail.com>
2019-05-21 19:10:09 -0300

    opa V

--- src/newtonRaphson/Teste.java
+++ src/newtonRaphson/Teste.java
@@ -1,26 +1,52 @@
 package newtonRaphson;
 
 import java.util.LinkedList;
+import java.util.Scanner;
 import java.util.Stack;
 
 public class Teste {
-
+	static double [] deposito = new double [10];
+	static int [] tempo = new int [10];
+	static double saldo;
+	static int t_s;
+	
+	
+	
 	static double newton(double epsilon) {
-		double [] deposito = new double [10];
-		int [] tempo = new int [10];
-		double saldo;
-		int t_s;
+		
 		Stack depositos = new Stack();
 		LinkedList lista = new LinkedList();
+		double j = 0.5;
+		double jk = 0.5;
+		double funcao = 0;
+		double derivada = 0;
 		
+		do {
+			j = jk;
+			for (int i = 0; i < 10; i++) {
+				funcao += deposito[i]*Math.pow(t_s-tempo[i], 1+j);
+				derivada +=(t_s-tempo[i])*deposito[i]*Math.pow(1+j, (t_s-tempo[i]-1));
+			}
+			jk = jk-(funcao/derivada);
+		} while (Math.abs(jk-j)>=epsilon);
 		
-		return 0;
+		return jk;
 	}
 	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner entrada = new Scanner(System.in);
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Digite o " + (i+1) + " depósito");
+			deposito[i] = Double.parseDouble(entrada.nextLine());
+			System.out.println("Digite o mês do " + (i+1) + " depósito" );
+			tempo[i] = Integer.parseInt(entrada.nextLine());
+		}
+		System.out.println("Digite o saldo");
+		saldo = Double.parseDouble(entrada.nextLine());
+		System.out.println("Digite o tf final");
+		t_s = Integer.parseInt(entrada.nextLine());
 	}
 
 }
