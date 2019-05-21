7e74b9ed471f5929337bee5d08293fa02a7fba6c
Todinho <gabrieltosi5@gmail.com>
2019-05-21 18:31:29 -0300

    Update Teste.java

--- src/newtonRaphson/Teste.java
+++ src/newtonRaphson/Teste.java
@@ -1,7 +1,23 @@
 package newtonRaphson;
 
+import java.util.LinkedList;
+import java.util.Stack;
+
 public class Teste {
 
+	static double newton(double epsilon) {
+		double [] deposito = new double [10];
+		int [] tempo = new int [10];
+		double saldo;
+		int t_s;
+		Stack depositos = new Stack();
+		LinkedList lista = new LinkedList();
+		
+		
+		return 0;
+	}
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
