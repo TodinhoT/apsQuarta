c72df1a0dfbfe977cca1ff3d1c36a247df897e77
F-Massaharu <39600408+F-Massaharu@users.noreply.github.com>
2019-05-22 13:39:14 -0300

    Update Teste.java

--- src/newtonRaphson/Teste.java
+++ src/newtonRaphson/Teste.java
@@ -5,23 +5,24 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class Teste {
-	static double[] deposito = new double[10];
-	static int[] tempo = new int[10];
-	static double saldo;
-	static int t_s;
+	static double[] deposito = {1000,1200,100,0,1100,0,900,0,0,0};
+	static int[] tempo = {3,4,5,6,7,8,9,10,11,12};
+	static double saldo = 5000;
+	static int t_s = 12;
 
 	static double newton(double epsilon) {
 
-		Stack depositos = new Stack();
-		LinkedList lista = new LinkedList();
+		Stack<Double> depositos = new Stack<Double>();
+		LinkedList<Double> listaJ = new LinkedList<Double>();
+		LinkedList<Double> listaJk = new LinkedList<Double>();
+		LinkedList<Double> listaVerif = new LinkedList<Double>();
 		double j = 0.5;
 		double jk = 0;
 		double funcao = 0;
 		double derivada = 0;
 		double verif;
-
+		
 		do {
-
 			for (int i = 1; i < 10; i++) {
 				if (i == 1) {
 					funcao = deposito[0] * Math.pow(1 + j, t_s - tempo[0]);
@@ -30,27 +31,36 @@ public class Teste {
 				funcao += deposito[i] * Math.pow(1 + j, t_s - tempo[i]);
 				derivada += (t_s - tempo[i]) * deposito[i] * Math.pow(1 + j, (t_s - tempo[i] - 1));
 			}
-
 			jk = j - ((funcao - saldo) / derivada);
-			verif = Math.abs(jk - j);
+			verif = Math.abs(jk - j);			
 			j = jk;
+			
+			listaJ.add(j);
+			listaJk.add(jk);
+			listaVerif.add(verif);
 		} while (verif >= epsilon);
+		System.out.println("Interacao ---------------------- Jk ------------------------------ Jk+1 -------------------------- |(Jk+1)-Jk| -----------------");
+		for (int i = 0; i < listaJ.size(); i++) {
 
+			System.out.println("   "+(i+1)+"     |-------------| " + listaJ.get(i) + " |-------------| " + listaJk.get(i) + " |-------------| " + listaVerif.get(i) +" |-------------|");
+		}
+		System.out.println("---------------------------------------------------------------------------------------------------------------------------------");
 		return jk;
 	}
 
 	public static void main(String[] args) {
 		Scanner entrada = new Scanner(System.in);
-		for (int i = 0; i < 10; i++) {
-			System.out.println("Digite o " + (i + 1) + " depósito");
-			deposito[i] = Double.parseDouble(entrada.nextLine());
-			System.out.println("Digite o mês do " + (i + 1) + " depósito");
-			tempo[i] = Integer.parseInt(entrada.nextLine());
-		}
-		System.out.println("Digite o saldo");
-		saldo = Double.parseDouble(entrada.nextLine());
-		System.out.println("Digite o tf final");
-		t_s = Integer.parseInt(entrada.nextLine());
+//		for (int i = 0; i < 10; i++) {
+//			System.out.println("Digite o " + (i + 1) + " depósito");
+//			deposito[i] = Double.parseDouble(entrada.nextLine());
+//			System.out.println("Digite o mês do " + (i + 1) + " depósito");
+//			tempo[i] = Integer.parseInt(entrada.nextLine());
+//		}
+//		System.out.println("Digite o saldo");
+//		saldo = Double.parseDouble(entrada.nextLine());
+//		System.out.println("Digite o tf final");
+//		t_s = Integer.parseInt(entrada.nextLine());
+		
 
 		System.out.println(newton(0.001));
 		entrada.close();
