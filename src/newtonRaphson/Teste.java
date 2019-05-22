af3b16090c0871ce60a1dda828f2d659b478d2e3
Todinho <gabrieltosi5@gmail.com>
2019-05-22 19:13:13 -0300

    Update Teste.java

--- src/newtonRaphson/Teste.java
+++ src/newtonRaphson/Teste.java
@@ -1,27 +1,65 @@
 package newtonRaphson;
 
+import java.text.DecimalFormat;
 import java.util.LinkedList;
 import java.util.Scanner;
 import java.util.Stack;
 
 public class Teste {
-	static double[] deposito = {1000,1200,100,0,1100,0,900,0,0,0};
-	static int[] tempo = {3,4,5,6,7,8,9,10,11,12};
+	// Declaracao das variaveis
+	static double[] deposito = { 1000, 1200, 100, 0, 1100, 0, 900, 0, 0, 0 };
+	static int[] tempo = { 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
 	static double saldo = 5000;
 	static int t_s = 12;
+	static double j = 0.5;
+	static double jk = 0;
+	static double funcao = 0;
+	static double derivada = 0;
+	static double verif;
+	static Stack<Double> saldoT = new Stack<Double>();
+	static Stack<Double> depositos = new Stack<Double>();
 
+	// Metodo que armazena os depositos acumulados em uma pilha e a coloca em ordem
+	static void armazenaDeposito() {
+		Stack<Double> temp = new Stack<Double>();
+		for (int i = 1; i < deposito.length; i++) {
+			if (i == 1) {
+				temp.push(deposito[0]);
+			}
+			temp.push(temp.peek() + deposito[i]);
+		}
+		for (int i = 0; i < deposito.length; i++) {
+			depositos.push(temp.pop());
+		}
+	}
+
+	// Metodo que armazena o saldo total em uma pilha e a coloca em ordem
+	static void armazenaSaldo() {
+		Stack<Double> temp = new Stack<Double>();
+		for (int i = 1; i < deposito.length; i++) {
+			if (i == 1) {
+				temp.push(deposito[0]);
+			}
+			temp.push(temp.peek() + (temp.peek() * jk) + deposito[i]);
+		}
+		for (int i = 0; i < deposito.length; i++) {
+			saldoT.push(temp.pop());
+		}
+	}
+
+	// metodo Newton solicitado
 	static double newton(double epsilon) {
-
-		Stack<Double> depositos = new Stack<Double>();
+		// Declaracao das variaveis
 		LinkedList<Double> listaJ = new LinkedList<Double>();
 		LinkedList<Double> listaJk = new LinkedList<Double>();
 		LinkedList<Double> listaVerif = new LinkedList<Double>();
-		double j = 0.5;
-		double jk = 0;
-		double funcao = 0;
-		double derivada = 0;
-		double verif;
-		
+		DecimalFormat df = new DecimalFormat("####.00");
+		double[] rendimentoAcumulado = new double[deposito.length];
+		double[] rendimentoMensal = new double[deposito.length];
+		// iniciando a lista com o primeiro juros
+		listaJ.add(j);
+
+		// Calculo da formula de NewtonRaphson
 		do {
 			for (int i = 1; i < 10; i++) {
 				if (i == 1) {
@@ -32,19 +70,78 @@ public class Teste {
 				derivada += (t_s - tempo[i]) * deposito[i] * Math.pow(1 + j, (t_s - tempo[i] - 1));
 			}
 			jk = j - ((funcao - saldo) / derivada);
-			verif = Math.abs(jk - j);			
+			verif = Math.abs(jk - j);
 			j = jk;
-			
+
 			listaJ.add(j);
 			listaJk.add(jk);
 			listaVerif.add(verif);
 		} while (verif >= epsilon);
-		System.out.println("Interacao ---------------------- Jk ------------------------------ Jk+1 -------------------------- |(Jk+1)-Jk| -----------------");
-		for (int i = 0; i < listaJ.size(); i++) {
 
-			System.out.println("   "+(i+1)+"     |-------------| " + listaJ.get(i) + " |-------------| " + listaJk.get(i) + " |-------------| " + listaVerif.get(i) +" |-------------|");
+		// Calculo do rendimento acumulado
+		armazenaDeposito();
+		armazenaSaldo();
+		for (int i = 0; i < rendimentoAcumulado.length; i++) {
+			if (i == 0) {
+				rendimentoAcumulado[i] = 0;
+			} else {
+				rendimentoAcumulado[i] = saldoT.peek() - depositos.peek();
+				depositos.pop();
+				saldoT.pop();
+			}
+
 		}
-		System.out.println("---------------------------------------------------------------------------------------------------------------------------------");
+
+		// Calculo do rendimento mensal
+		armazenaSaldo();
+		for (int i = 0; i < rendimentoMensal.length; i++) {
+			if (i == 0) {
+				rendimentoMensal[i] = 0;
+			} else {
+				rendimentoMensal[i] = saldoT.peek() * jk;
+				saldoT.pop();
+			}
+		}
+
+		// Print de todos os juros
+		listaJ.removeLast();
+		System.out.println(
+				"Interacao ---------------------- Jk ------------------------------------ Jk+1 ------------------------------ |(Jk+1)-Jk| --------");
+		for (int i = 0; i < listaJ.size(); i++) {
+			if (i == 0) {
+				System.out.println(
+						"   " + (i + 1) + "    |-------------|" + listaJ.get(i) + "               \t|-------------|\t"
+								+ listaJk.get(i) + "\t|-------------|\t" + listaVerif.get(i) + "\t|");
+			} else
+				System.out.println("   " + (i + 1) + "    |-------------|" + listaJ.get(i) + "\t|-------------|\t"
+						+ listaJk.get(i) + "\t|-------------|\t" + listaVerif.get(i) + "\t|");
+		}
+		System.out.println(
+				"---------------------------------------------------------------------------------------------------------------------------------");
+		System.out.println("");
+
+		// Print dos dados de todos os dados
+		armazenaDeposito();
+		armazenaSaldo();
+		System.out.println("");
+
+		System.out.println(
+				"Mes ------------------ Deposito ----------- DepositoAcumulado ---------- Saldo ----------- RendimentoMensal ------ RendimentoAcumulado");
+		for (int i = 0; i < t_s; i++) {
+			if (i == 0 || i == 1) {
+				System.out.println("  " + (i + 1) + "\t|-------------|" + 0 + "\t|-------------|\t" + 0
+						+ "\t|-------------|\t" + 0 + "\t|-------------|\t" + 0 + "\t|-------------|\t" + 0 + "\t|");
+			} else {
+				System.out.println("  " + (i + 1) + "\t|-------------|" + deposito[i - 2] + "\t|-------------|\t"
+						+ df.format(depositos.peek()) + "\t|-------------|\t" + df.format(saldoT.peek())
+						+ "\t|-------------|\t" + df.format(rendimentoMensal[i - 2]) + "\t|-------------|\t"
+						+ df.format(rendimentoAcumulado[i - 2]) + "\t|");
+				depositos.pop();
+				saldoT.pop();
+			}
+		}
+		System.out.println(
+				"---------------------------------------------------------------------------------------------------------------------------------");
 		return jk;
 	}
 
@@ -60,7 +157,6 @@ public class Teste {
 //		saldo = Double.parseDouble(entrada.nextLine());
 //		System.out.println("Digite o tf final");
 //		t_s = Integer.parseInt(entrada.nextLine());
-		
 
 		System.out.println(newton(0.001));
 		entrada.close();
