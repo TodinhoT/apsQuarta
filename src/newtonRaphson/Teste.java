ae7c1df0b41cffd933acfb5a349777f9a539eeef
Todinho <gabrieltosi5@gmail.com>
2019-05-21 22:11:56 -0300

    Update Teste.java

--- src/newtonRaphson/Teste.java
+++ src/newtonRaphson/Teste.java
@@ -15,24 +15,27 @@ public class Teste {
 		Stack depositos = new Stack();
 		LinkedList lista = new LinkedList();
 		double j = 0.5;
-		double jk = 0.5;
+		double jk = 0;
 		double funcao = 0;
 		double derivada = 0;
+		double limite;
 
 		do {
-			funcao += deposito[0] * Math.pow(1 + j, t_s - tempo[0]);
-			derivada += (t_s - tempo[0]) * deposito[0] * Math.pow(1 + j, (t_s - tempo[0] - 1));
-			
-			for (int i = 0; i < 10; i++) {
+			funcao = deposito[0] * Math.pow(1 + j, t_s - tempo[0]);
+			derivada = (t_s - tempo[0]) * deposito[0] * Math.pow(1 + j, (t_s - tempo[0] - 1));
+						
+			for (int i = 1; i < 10; i++) {
 				funcao += deposito[i] * Math.pow(1 + j, t_s - tempo[i]);
 				derivada += (t_s - tempo[i]) * deposito[i] * Math.pow(1 + j, (t_s - tempo[i] - 1));
 			}
 
-			jk = jk - (funcao / derivada);
-
+			jk = j - ((funcao-saldo) / derivada);
+			limite = Math.abs(jk - j);
+			
 			System.out.println(jk);
+			System.out.println(limite);
 			j = jk;
-		} while (Math.abs(jk - j) >= epsilon);
+		} while (limite >= epsilon);
 
 		return jk;
 	}
